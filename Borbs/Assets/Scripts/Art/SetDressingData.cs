c00525e7141fa01bed64d74fa9dfdfe323d55ff0
yoska05 <yoskamain@gmail.com>
2021-01-30 15:09:41 +0100

    Terrain

---
+++ Borbs/Assets/Scripts/Art/SetDressingData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+[CreateAssetMenu(menuName = "HaH/SetdressingData" , order = 100)]
+
+public class SetDressingData : ScriptableObject
+{
+    public List<GameObject> gameObjects = new List<GameObject>();
+    public float Desity;
+
+    public float yRanRotation = 360;
+    public float xRanRotation = 7;
+    public float zRanRotation = 7;
+
+    public float minScale = 0.8f;
+    public float maxScale = 1f;
+}
---
+++ Borbs/Assets/Scripts/Art/SetDressingPrefab.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetDressingPrefab : MonoBehaviour
+{
+    public SetDressingData SetData;
+    public void Dress()
+    {
+        float scale = Random.Range(SetData.minScale , SetData.maxScale);
+        transform.localScale = new Vector3(scale , scale , scale);
+        transform.eulerAngles = new Vector3(Random.Range(-SetData.xRanRotation , SetData.xRanRotation) , Random.Range(-SetData.yRanRotation , SetData.yRanRotation) , Random.Range(-SetData.zRanRotation , SetData.zRanRotation));
+    }
+
+    public void Awake()
+    {
+        if (SetData == null)
+        {
+            Debug.LogWarning("Unessecary Setdressing Randomizer, If you intent to use these make sure to add a setdata or remove this component");
+            return;
+        }
+        //Dress();
+        Destroy(this);
+    }
+}
