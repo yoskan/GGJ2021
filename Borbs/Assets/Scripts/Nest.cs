cf86cd828af3be3bd2b3625b878a03781c9d5971
yoska05 <yoskamain@gmail.com>
2021-01-30 17:10:03 +0100

    Nest

--- Borbs/Assets/Scripts/Nest.cs
+++ Borbs/Assets/Scripts/Nest.cs
@@ -19,7 +19,7 @@ public class Nest : MonoBehaviour
         foreach (GameObject player in Pla)
         {
             Players.Add(player.GetComponent<PlayerCharacterScript>());
-            if (players.ID == NestID)
+            if (player.ID == NestID)
             {
                 Myborb = player.GetComponent<PlayerCharacterScript>();
             }
